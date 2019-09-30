10be62588585d3ac43e6a4cdc62a528e8cb2aed5
User from Noszony <nasz_email_do_github>
2019-09-30 16:07:08 +0200

    Second Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,31 @@
 package com.kodilla;
 
 public class Calculator {
+
+    private int A;
+    private int B;
+
+    public Calculator(){
+        this.A = A;
+        this.B = B;
+    }
+
+    public void sum(int A,int B){
+        int sum= A+B;
+        System.out.println("Result is: "+sum);
+    }
+    public void sub(int A,int B){
+        int sub = A-B;
+        System.out.println("Result is: "+sub);
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator =  new Calculator();
+        Calculator calculator1 =  new Calculator();
+
+        calculator.sum(15,7);
+        calculator1.sub(25,5);
+
+    }
+
 }
